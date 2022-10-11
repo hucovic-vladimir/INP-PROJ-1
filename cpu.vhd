eecf20986eb612fe4904a03c51a5039ce68ddb97
Vladimír Hucovič <hucovic.vladimir@gmail.com>
2022-10-11 21:50:38 +0200

    Added multiplexors and registers

--- cpu.vhd
+++ cpu.vhd
@@ -1,7 +1,7 @@
 -- cpu.vhd: Simple 8-bit CPU (BrainFuck interpreter)
 -- Copyright (C) 2022 Brno University of Technology,
 --                    Faculty of Information Technology
--- Author(s): jmeno <login AT stud.fit.vutbr.cz>
+-- Author(s): Vladimír Hucovič <xhucov00@stud.fit.vutbr.cz>
 --
 library ieee;
 use ieee.std_logic_1164.all;
@@ -41,7 +41,113 @@ end cpu;
 --                      Architecture declaration
 -- ----------------------------------------------------------------------------
 architecture behavioral of cpu is
+
+--- Program Counter signals
+
+signal PC : std_logic_vector(12 downto 0);
+signal PC_inc : std_logic;
+signal PC_dec : std_logic;
+
+
+--- Loop Counter signals
+
+signal CNT : std_logic_vector(12 downto 0);
+signal CNT_inc : std_logic;
+signal CNT_dec : std_logic;
+
+
+--- Memory pointer signals
+
+signal PTR : std_logic_vector(12 downto 0);
+signal PTR_inc : std_logic;
+signal PTR_dec : std_logic;
+
+--- Multiplexor selector signals
+
+signal mx1_sel : std_logic;
+signal mx2_sel : std_logic_vector(1 downto 0);
+
+
+
 begin
 
-end behavioral;
 
+
+
+reg_PC: process (RESET, CLK)
+begin
+	if(RESET = '1') then
+		PC <= (others => '0');
+	elsif (CLK'event) and (CLK = '1') then
+		if(PC_inc='1') then
+			PC <= PC + 1;
+		elsif(PC_dec='1') then
+			PC <= PC - 1;
+		end if;
+	end if;
+end process;
+
+
+
+reg_CNT: process (RESET, CLK)
+begin
+	if(RESET = '1') then
+		CNT <= (others => '0');
+	elsif (CLK'event) and (CLK = '1') then
+		if(CNT_inc='1') then
+			CNT <= CNT + 1;
+		elsif(CNT_dec='1') then
+			CNT <= CNT - 1;
+		end if;
+	end if;
+end process;
+
+
+reg_PTR: process (RESET, CLK)
+begin
+	if(RESET = '1') then
+		PTR <= (others => '0');
+	elsif (CLK'event) and (CLK = '1') then
+		if(PTR_inc='1') then
+			PTR <= PTR + 1;
+		elsif(PTR_dec='1') then
+			PTR <= PTR - 1;
+		end if;
+	end if;
+end process;
+
+
+mx1: process(CLK, RESET, mx1_sel)
+begin
+	if(RESET = '1') then 
+		DATA_ADDR <= (others => '0');
+	elsif (CLK'event and CLK='1') then
+		case mx1_sel is
+			when '0' => DATA_ADDR <= PC;
+			when '1' => DATA_ADDR <= PTR;
+			when others => DATA_ADDR <= (others => '0');
+		end case;
+	end if;			
+end process;
+
+mx2: process(CLK, RESET, mx2_sel)
+begin
+	if(RESET = '1') then
+		DATA_WDATA <= (others => '0');
+	elsif (CLK'event and CLK='1') then
+		case mx2_sel is
+			when "00" => DATA_WDATA <= IN_DATA;
+			when "01" => DATA_WDATA <= (DATA_RDATA - 1);
+			when "10" => DATA_WDATA <= (DATA_RDATA + 1);
+			when others => DATA_WDATA <= (others => '0');
+		end case;
+	end if;
+
+	--DATA_ADDR <= PC when (mx1_sel = '0')
+	--				else CNT;
+end process;
+DATA_WDATA <= IN_DATA when (mx2_sel = "00") else
+			(DATA_RDATA - 1) when (mx2_sel = "01") else
+			(DATA_RDATA + 1) when (mx2_sel = "10") else
+			(others => '0');
+end behavioral;
